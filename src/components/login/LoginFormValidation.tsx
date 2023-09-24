a36678428688c62973384b15c64a23196e4dd103
williammlekush <56037586+williammlekush@users.noreply.github.com>
2023-09-23 22:25:33 -0500

    fix: export something from LoginFormValidation.tsx

---
+++ src/components/login/LoginFormValidation.tsx
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+const LoginValidationSchema = {
+   username: yup.string(),
+   password: yup.string(),
+}
+
+export const LoginSchema = () => yup.object(LoginValidationSchema);
